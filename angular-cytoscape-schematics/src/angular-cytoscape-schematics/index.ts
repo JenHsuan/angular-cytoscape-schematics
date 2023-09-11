2d89f4a7121ac9aa78fe5ad490ae74d25124c0f5
Sean Hsieh <hsiehs@fortinet.com>
2023-09-11 16:11:07 +0800

    Update

--- angular-cytoscape-schematics/src/angular-cytoscape-schematics/index.ts
+++ angular-cytoscape-schematics/src/angular-cytoscape-schematics/index.ts
@@ -1,9 +1,23 @@
 import { Rule, SchematicContext, Tree } from '@angular-devkit/schematics';
 
+/*
+ * 1. Add json files to assets folder
+ * 2. Update angular.json
+ * 3. Update package.json
+ * 4. Add base folder
+ */
+export function setupCytoscapeProject(_options: any): Rule {
+  return (tree: Tree, _context: SchematicContext) => {
+    return tree;
+  };
+}
 
-// You don't have to export the function as default. You can also have more than one rule factory
-// per file.
-export function angularCytoscapeSchematics(_options: any): Rule {
+/*
+ * 1. Add new folder with the specific name
+ * 2. Update angular.json
+ * 3. Update package.json
+ */
+export function createNewComponent(_options: any): Rule {
   return (tree: Tree, _context: SchematicContext) => {
     return tree;
   };
